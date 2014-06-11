bebdce32eeaa85620c474cb3bae77137a34093b2
Fernando Godinho <fppgodinho@gmail.com>
2014-06-11 10:44:18 +0100

    IntelliJ Commit

--- src/net/darkhounds/tr/craftingbuddy/client/js/services/blueprint.js
+++ src/net/darkhounds/tr/craftingbuddy/client/js/services/blueprint.js
@@ -3,7 +3,7 @@ trcraftingbuddy.service('blueprint', ['observable', 'data', 'analytics', '$rootS
     var service             = observable.create({});
     var blueprint           = {};
     service.initialize      = function(id)                                      {
-        analytics.register('blueprint/initialize/' + id);
+        analytics.register('blueprint/initialize/' + (id || 'new'));
         blueprint       = {id: id, parent: null};
         Object.defineProperty(blueprint, 'addRecipe',                           {
             enumerable:     false,
